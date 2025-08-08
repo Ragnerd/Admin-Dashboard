7eb301e21114a1b8724e67f1b2cf9087ad2df918
Ragnerd <foysalakondo97@gmail.com>
2025-08-09 02:01:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+const App = () => {
+  return <h1 className="underline text-5xl">App</h1>;
+};
+
+export default App;
