ee7f73030fc8332f96838fdbb90d06433871e59a
Ragnerd <foysalakondo97@gmail.com>
2025-08-10 02:28:24 +0600

    commiting app component

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+// Import the license registration
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
